c361d5d312163572489ba0250e158d1a43322040
Amiron28 <fotolia505@gmail.com>
2022-03-02 15:00:55 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,194 @@
+// let a = 10
+// let b = 20
+// let c = a + b
+// let d = c ** 2
+// let e = c / 3
+// let f = e % 2
+// let g = f === 0
+// let h = g + b
+// let j = typeof(h) + 2
+// let k = j - 4
+
+// console.log(k);
+
+
+// concat
+
+
+// let name = 'Alex'
+// let surname = 'Adams'
+// let age = 15
+
+
+// //конкатинация
+// console.log('Hellow world my name is ' + name + ' ' + surname + ' ' + age + ' I would like married and I am study at scholl number 33 my brothers name is Nazar my mothers name is Lola my fathers name is Xafiz ');
+
+// //интерполяция 
+// console.log(`Hellow world my name is ${name} ${surname}`);
+
+
+
+
+// let a = 'alex'
+// let b = 'Alex'
+
+
+// console.log(a == b);
+
+
+
+
+
+
+// let a = prompt('')
+
+// console.log(eval(a));
+
+
+
+//increment
+//decrement
+
+
+
+
+
+
+///d/z
+
+// let admin = 'Джон'
+// let name = 'Джон'
+
+
+// = 890
+
+
+// let invoice = 5500
+// let stock = 4200
+
+// console.log(balance - car - food + invoice + stock)
+
+
+// /////////
+// console.log(admin);
+
+
+
+// /////
+// let balance = 25000
+
+// let car = 4480
+// let food
+
+
+
+
+///dz-2
+
+
+// let a = prompt()
+
+// eval()
+
+// console.log(eval(a));
+
+
+
+
+
+// console.log(text.length);
+
+
+
+// let a = 'hellow world'
+
+
+// console.log(a.toUpperCase());
+
+
+
+// let a = 'HELLOW WORLD'
+
+
+// console.log(a.toLowerCase());
+
+
+
+// let text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo atque recusandae rerum ad voluptate ab ratione ea dolorum eum nostrum quia accusantium vitae, officiis, at aliquam, laudantium magnam similique nihil tenetur quibusdam in est quos impedit'
+
+
+// console.log(text.search('m'));
+
+
+
+// console.log(text.slice(22, 23).toUpperCase() + text.slice(4, 5));
+
+
+
+
+// let a = 'aaa@bbb@ccc'
+
+// a = a.replaceALL('@', '!', )
+// console.log(a);
+
+
+// let a = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa exercitationem provident cupiditate at laudantium, doloremque asperiores soluta facere possimus.'
+
+
+// console.log(a.split(''));
+
+
+// console.log(window.location.href.split('/'));
+
+
+
+
+
+///d/z/2
+
+// let str = 'aaa@bbb@ccc'
+
+// str = str.replace('@', '!')
+// str = str.replace('@', '!')
+
+
+// console.log(str);
+
+// //////
+
+// let str_two = 'HELLOWORLD'
+
+// console.log(str_two.slice(0, 1).toUpperCase() + str_two.slice(1, 5).toLocaleLowerCase() + ' ' + str_two.slice(5).toLowerCase());
+
+// //////
+
+// let c = 'Hello , it is HTML'
+
+// console.log(c.slice(0, 5) + ' ' + c.slice(8, 10) + ' ' + c.slice(11, 13) + ' not JS');
+
+// //////////
+
+// let a = 'alex'
+
+// console.log(a.slice(0, 1).toUpperCase() + a.slice(1, 2) + a.slice(2, 3) + a.slice(3, 4));
+
+// ///////
+
+// let b = 'Math.random'
+
+// console.log(b.slice(0, 4) + ' ' + b.slice(5));
+
+// /////////
+
+// let text = 'Lex Luter has a big suit'
+
+// console.log(text.slice(11, 12).toUpperCase() + text.slice(0, 1).toUpperCase() + text.slice(1, 2) + text.slice(2, 3));
+
+// ////////
+
+
+
+
+// let math = Math.random().toString()
+
+// console.log(math);
