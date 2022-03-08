affa22457f8d8328819691870440ed2a6581ac23
Amiron28 <fotolia505@gmail.com>
2022-03-08 11:11:58 +0500

    new style wac done

--- script.js
+++ script.js
@@ -4,20 +4,20 @@
 ////d/z
 
 
-let a = prompt('Введите число')
+// let a = prompt('Введите число')
 
-if (a % 2 == 0) {
-    console.log(alert('четное число'));
-} else {
-    console.log(alert('нечетное число'));
-}
+// if (a % 2 == 0) {
+//     console.log(alert('четное число'));
+// } else {
+//     console.log(alert('нечетное число'));
+// }
 
 
 
-let name = prompt('Введите имя')
+// let name = prompt('Введите имя')
 
-if (name === 'Alex' || name === 'alex' || name === 'Алекс' || name === 'алекс' || name === 'ALex' || name === 'ALEX' || name === 'АЛЕКС' || name === 'ALeX' || name === 'ALEx' || name === 'АЛЕКс') {
-    console.log(alert('проходите'));
-} else {
-    console.log(alert('вы не подходите по имени'));
-}
+// if (name === 'Alex' || name === 'alex' || name === 'Алекс' || name === 'алекс' || name === 'ALex' || name === 'ALEX' || name === 'АЛЕКС' || name === 'ALeX' || name === 'ALEx' || name === 'АЛЕКс') {
+//     console.log(alert('проходите'));
+// } else {
+//     console.log(alert('вы не подходите по имени'));
+// }
