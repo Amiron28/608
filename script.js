993135158fe4293451d927fece837c5837348cc2
Amiron28 <fotolia505@gmail.com>
2022-03-10 22:11:26 +0500

    I have started doing homework

--- script.js
+++ script.js
@@ -1,94 +1,58 @@
-//08.03/2022
+// 10.03.2022
 
 
+let arr = [
+    {
+        name: ' apple',
+        price: 15000
+    },
+    {
+        name: 'mandarin',
+        price: 20000
+    },
+    {
+        name: ' Avokado',
+        price: 80000
+    },
+    {
+        name: 'banana',
+        price: 17000
+    },
+    {
+        name: 'snickers',
+        price: 5000
+    },
+    {
+        name: 'tedy',
+        price: 600000
+    },
+    {
+        name: ' Abrikos',
+        price: 5000
+    },
+    {
+        name: ' Ayva',
+        price: 600000
+    },
+    {
+        name: ' Ariko',
+        price: 600000
+    },
+    {
+        name: ' Arbuz',
+        price: 600000
+    },
+    {
+        name: ' AMIR',
+        price: NaN
+    },
+]
 
-console.log('Task : 1');
+let aPr = arr.filter(item => item.name.trim()[0] === 'A' || item.name.trim()[0] === 'a')
 
-let cars = [car_1 = 6500, car_2 = 7000, car_3 = 20000, car_3 = 7001, car_4 = 21000, car_5 = 6000, car_6 = 100000]
+console.log(aPr);
 
-let cheap = []
-let expensive = []
+let searchingPr = prompt('Введите продукт')
 
-cars.filter(item => {
-    if ((item) <= '7000') {
-        cheap.push(item)
-    } else if ((item) > '7000') {
-        expensive.push(item)
-    }
-})
-
-console.log(cheap, expensive);
-
-
-
-console.log('Task : 2');
-
-
-let arr = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22]
-
-let num = []
-let string = []
-let bool = []
-let obj = []
-
-arr.filter(item => {
-    if (typeof (item) === 'number') {
-        num.push(item)
-    } else if (typeof (item) === 'string') {
-        string.push(item)
-    } else if (typeof (item) === 'boolean') {
-        bool.push(item)
-    } else if (typeof (item) === 'object') {
-        obj.push(item)
-    }
-})
-
-
-console.log(num, string, bool, obj);
-
-
-console.log('Task : 3 and 5');
-
-let arr_1 = [1, 3, false, 'hello', 24, 'world', undefined, null, 'error', 22]
-
-let numb = []
-let strin = []
-let boo = []
-let obje = []
-
-arr_1.filter(item => {
-    if (typeof (item) === 'number') {
-        numb.push(item)
-    } else if (typeof (item) === 'string') {
-        strin.push(item)
-    } else if (typeof (item) === 'boolean') {
-        boo.push(item)
-    } else if (typeof (item) === 'object') {
-        obje.push(item)
-    }
-})
-
-if (numb.length >= 5) {
-    console.log('Good');
-} else {
-    console.log('Bad');
-}
-
-console.log(numb);
-
-
-
-
-console.log('Task : 4');
-
-let index = +prompt();
-
-let aray = ['Aleksey', 'Kartoshka', 'Margarita', 'Morgenshtern', 'Monica'];
-
-aray.splice(index, index)
-console.log(aray);
-
-
-
-
+console.log(arr[searchingPr]);
 
--- script_2.js
+++ script_2.js
@@ -386,11 +386,11 @@
 
 // console.log(arr[0] + arr[2] + arr[3].b + arr[4][1] + arr[6] + arr[7] + arr[9].a + arr[10][0] + arr[11][0] + arr[11][1][0][0] + arr[11][2]);
 
-//методы массивов
+// методы массивов
 
 
 
-//методы добавления
+// методы добавления
 
 // let arr_names = ['Gulnaz', 'Karina', 'Kaneki', 'Amir', 'Daler', 'Damir', 'Daniel']
 
@@ -410,7 +410,7 @@
 // console.log(arr_names);
 
 
-//методы поиска
+// методы поиска
 
 
 // let arr_names = ['Gulnaz', 'Karina', 'Kaneki', 'Damir', 'Amir', 'Daler', 'Damir', 'Daniel', 'Damir',]
@@ -452,5 +452,98 @@
 
 
 // console.log(num, string, bool, obj);
+
+
+// // 08.03/2022
+
+
+
+// console.log('Task : 1');
+
+// let cars = [car_1 = 6500, car_2 = 7000, car_3 = 20000, car_3 = 7001, car_4 = 21000, car_5 = 6000, car_6 = 100000]
+
+// let cheap = []
+// let expensive = []
+
+// cars.filter(item => {
+//     if ((item) <= '7000') {
+//         cheap.push(item)
+//     } else if ((item) > '7000') {
+//         expensive.push(item)
+//     }
+// })
+
+// console.log(cheap, expensive);
+
+
+
+// console.log('Task : 2');
+
+
+// let arr = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22]
+
+// let num = []
+// let string = []
+// let bool = []
+// let obj = []
+
+// arr.filter(item => {
+//     if (typeof (item) === 'number') {
+//         num.push(item)
+//     } else if (typeof (item) === 'string') {
+//         string.push(item)
+//     } else if (typeof (item) === 'boolean') {
+//         bool.push(item)
+//     } else if (typeof (item) === 'object') {
+//         obj.push(item)
+//     }
+// })
+
+
+// console.log(num, string, bool, obj);
+
+
+// console.log('Task : 3 and 5');
+
+// let arr_1 = [1, 3, false, 'hello', 24, 'world', undefined, null, 'error', 22]
+
+// let numb = []
+// let strin = []
+// let boo = []
+// let obje = []
+
+// arr_1.filter(item => {
+//     if (typeof (item) === 'number') {
+//         numb.push(item)
+//     } else if (typeof (item) === 'string') {
+//         strin.push(item)
+//     } else if (typeof (item) === 'boolean') {
+//         boo.push(item)
+//     } else if (typeof (item) === 'object') {
+//         obje.push(item)
+//     }
+// })
+
+// if (numb.length >= 5) {
+//     console.log('Good');
+// } else {
+//     console.log('Bad');
+// }
+
+// console.log(numb);
+
+
+
+
+// console.log('Task : 4');
+
+// let index = +prompt();
+
+// let aray = ['Aleksey', 'Kartoshka', 'Margarita', 'Morgenshtern', 'Monica'];
+
+// aray.splice(index, index)
+// console.log(aray);
+
+
 
 
