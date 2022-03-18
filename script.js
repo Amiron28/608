945124f68d1726fb3d835b7c8a81d8bf08bc2711
Amiron28 <fotolia505@gmail.com>
2022-03-18 14:23:31 +0500

    d/z

--- script.js
+++ script.js
@@ -1,70 +1,3 @@
-
-// let user = {
-//     name: 'Amir',
-//     surname: 'Nazarov',
-//     age: 15,
-//     isMarried: true,
-//     hairColor: 'orange'
-// }
-
-// let money = {
-//     auto: 30000,
-//     food: 50000,
-//     friends: 100000,
-//     hairColor: 'red',
-//     expenses: [
-//         {
-//             house: 100000
-//         }
-//     ]
-// }
-
-
-// let assign = Object.assign(user, money)
-// let values = Object.values(assign)
-
-
-// let num = []
-// let str = []
-// let bool = []
-
-
-// let filter = values.filter(item => {
-//     if (typeof (item) === 'number') {
-//         num.push(item)
-//     } else if (typeof (item) === 'string') {
-//         str.push(item)
-//     } else if (typeof (item) === 'boolean') {
-//         bool.push(item)
-//     }
-// })
-
-// console.log(num, str, bool);
-
-
-// if (Math.max(num.length) > Math.max(str.length) || Math.max(num.length) > Math.max(bool.length)) {
-//     console.log(num + ' больше ');
-// } else if (Math.max(str.length) > Math.max(num.length) || Math.max(str.length) > Math.max(bool.length)) {
-//     console.log(str + ' больше ');
-// } else if (Math.max(bool.length) > Math.max(num.length) || Math.max(bool.length) > Math.max(str.length)) {
-//     console.log(bool + ' больше ');
-// }
-
-/-------------------------------------------------------------------/
-
-//17.03.2022 год
-
-// let arr = ['Gulnaz', 'Parviz', 'Daler', 'Amir', 'Daniel', 'Davlat', 'Damir', 'Jurabek', 'Jaxongir', 'Said', 'Karina']
-
-// let name = prompt('имя')
-
-// for (let item of arr) {
-//     if (item[0] === name) {
-//         console.log(item);
-//     }
-// }
-
-
 let arr = [
     {
         id: 1,
@@ -121,10 +54,10 @@ let arr = [
 let discount = 20
 
 for (let item of arr) {
-    item.price = Number
+    let num = Number(arr.price)
 
-    item.price === item.price - (discount * item.price / 100)
-        console.log(item.price);
-    }
+    item.price = item.price - (discount * item.price / 100)
+    console.log(item.price);
 }
+
 
