6bb7df85761f91d22b77f2b70d3382003ab75aa5
Amiron28 <fotolia505@gmail.com>
2022-03-22 11:07:44 +0500

    d/z

--- script.js
+++ script.js
@@ -11,13 +11,3 @@ for (let i = 1; i <= 9; i++) {
     for (let k = 1; k <= 3; k++)
         document.write(i)
 }
-
-document.write(`<br>`)
-document.write(`<br>`)
-
-for (k = 1; k <= 5; k++) {
-    for (let i = 1; i <= 5; i++){
-        document.write(`* `)
-    }
-        // document.write(`* <br>`)
-}
