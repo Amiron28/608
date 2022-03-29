8a778078bab793de2790db80764aadd20eb8003c
Amiron28 <fotolia505@gmail.com>
2022-03-29 07:21:01 +0500

    d/z

--- script.js
+++ script.js
@@ -1,62 +1,78 @@
-let arr_cars = [
-    {
-        name: 'malibu',
-        price: 30000
-    },
-    {
-        name: 'matiz',
-        price: 3000
-    },
-    {
-        name: 'spark',
-        price: 7500
-    },
-    {
-        name: 'gentra',
-        price: 140000
-    },
-    {
-        name: 'santaferro',
-        price: 80000
-    },
-    {
-        name: 'tahoe',
-        price: 80000
-    },
-    {
-        name: 'tesla model y',
-        price: 30000
-    }
-]
+// Task 1
+// let age = (status) => {
+//     if (status >= 18) {
+//         alert('welcome')
+//     } else {
+//         alert('bye')
+//     }
 
-let question_1 = prompt('какое название машины')
-let question_2 = +prompt('какая цена')
-let question_3 = +prompt('какого года')
+// }
 
-if (question_3 < 2010) {
-    question_2 = question_2 - (question_2 * 20 / 100)
-}
-
-let add = confirm(`
-    название машины: ${question_1}
-    цена машины: ${question_2}
-    год машины: ${question_3}
-
-    Вы уверены что хотите добавить ${question_1}
-`)
+// age(prompt('какой ваш возвраст ?'))
 
 
-let add_machine = {
-    name: question_1,
-    price: question_2,
-    year: question_3
-}
+
+// Task 2
+// let numbers = (arg, arg2) => {
+//     console.log(`
+//     ${arg} * ${arg2} = ${arg * arg2}
+//     `);
+//     console.log(`
+//     ${arg} % ${arg2} = ${arg % arg2}
+//     `);
+//     console.log(`
+//     ${arg} - ${arg2} = ${arg - arg2}
+//     `);
+//     console.log(`
+//     ${arg} / ${arg2} = ${arg / arg2}
+//     `);
+//     console.log(`
+//     ${arg} ** ${arg2} = ${arg ** arg2} 
+//     `);
+//     console.log(`
+//     ${arg} + ${arg2} = ${arg + arg2} 
+
+//     `);
+// }
+
+// numbers(a = +prompt('a'), b = +prompt('b'))
 
 
-if (add === true) {
-    arr_cars.push(add_machine)
-} else {
-    alert('понятно')
-}
 
-console.log(arr_cars);
+// console.log('Task 3:');
+// let leng = (name, name2, name3) => {
+//     if(name.length > name2.length && name.length > name3.length){
+//         console.log(name);
+//     }else if (name2.length > name.length && name2.length > name3.length){
+//         console.log(name2);
+//     }else if (name3.length > name.length && name3.length > name2.length){
+//         console.log(name3);
+//     }
+// }
+
+// leng('Amir', 'Damir', 'Jurabek')
+
+
+
+// Task 4
+// let MinNumber = (number_1, number_2) => {
+//     alert(Math.min(number_1,number_2))
+// }
+
+// MinNumber(a=prompt('первое число :'), b= prompt('второе число :'))
+
+
+
+// Task 5 
+// let proportion = (numberOne, percent) => {
+    // confirm(`
+//    ${numberOne} = 100%
+    // x = ${percent + ' %'} 
+//    `)
+// 
+    // alert(`
+    // x = ${numberOne} * ${percent} / 100 = ${numberOne * percent / 100 + ' %'}
+//    `)
+// }
+
+// proportion(a = prompt('первое число :'), c = prompt('процент второго числа :'))
