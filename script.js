d4c94cf9bca4bcaef332a74bde0511e5153a9d23
Amiron28 <fotolia505@gmail.com>
2022-04-25 17:43:45 +0500

    d/z

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector('.btn-1')
+let form = document.querySelector('.form')
+
+btn.onclick = () => {
+   
+}
+
+
+// form.style.display = "none"
+// form.style.display = "none"
+//
+// form.style.display = "block"
