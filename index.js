325d4e6b471e9382d4e7dd8196846824175fccd4
Amiron28 <fotolia505@gmail.com>
2022-05-02 20:42:43 +0500

    d/z

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-// let input = document.querySelector('input')
-// let label = document.querySelector('label')
-
-// input.onkeyup = () => {
-//     let a = input.value
-//     let b = String(+a)
-
-//     if(a === b){
-//         input.style.border = "2px red solid"
-
-//     }else{
-//         input.style.border = "2px green solid"
-//     }
-// }
-
-/------------------------------------------------------------------------------/
-
 let form = document.forms.register
 let block = document.querySelector('.block')
 let h1 = document.querySelectorAll('h1')
+let inputs = document.querySelectorAll('input')
+let ok = document.querySelector('.OK')
+let cansel = document.querySelector('.cansel')
 
-
+console.log(ok, cansel);
 
 form.onsubmit = (event) => {
     event.preventDefault()
 
+    for (let item of inputs) {
+        if (item.value.length > 0) {
+            submit()
+            item.style.border = "2px solid green"
+            block.style.display = "block"
+
+        } else {
+            item.value.innerHTML = "eroor"
+            block.style.display = "none"
+            cansel.style.display = "block"
+            ok.style.display = "block"
+            item.style.border = "2px solid red"
+            break
+        }
+    }
+}
+
+ok.onclick = () => { 
+    alert('succses')
+}
+
+cansel.onclick = () => { 
+    alert('понятно')
+    
+}
+
+function submit() {
     let user = {}
 
     let fm = new FormData(form)
@@ -34,6 +47,7 @@ form.onsubmit = (event) => {
 
     showUser(user)
 }
+
 
 function showUser(data) {
     let children = h1
