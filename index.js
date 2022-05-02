1b0855f0d7f66fba8ee21552d2a9ad4bdf8a47cb
Amiron28 <fotolia505@gmail.com>
2022-05-02 07:08:15 +0500

    d/z

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-let input = document.querySelector('input')
-let label = document.querySelector('label')
+// let input = document.querySelector('input')
+// let label = document.querySelector('label')
 
-input.onkeyup = () => {
-    if (+input.value === 'NaN') {
-        label.innerHTML = "не пиши  цифры по братски"
-        input.style.border = "2px red solid"
-    } else {
-        input.style.border = "2px solid green"
-        label.innerHTML = ""
+// input.onkeyup = () => {
+//     let a = input.value
+//     let b = String(+a)
+
+//     if(a === b){
+//         input.style.border = "2px red solid"
+
+//     }else{
+//         input.style.border = "2px green solid"
+//     }
+// }
+
+/------------------------------------------------------------------------------/
+
+let form = document.forms.register
+let block = document.querySelector('.block')
+let h1 = document.querySelectorAll('h1')
+
+
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    let user = {}
+
+    let fm = new FormData(form)
+
+    fm.forEach((value, key) => {
+        user[key] = value
+    })
+
+    showUser(user)
+}
+
+function showUser(data) {
+    let children = h1
+
+    for (let item of children) {
+        for (let key in data) {
+            if (item.getAttribute('name') == key) {
+                item.innerHTML = `${key}: ${data[key]}`
+            }
+        }
     }
+
 }
