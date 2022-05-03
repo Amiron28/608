968dd4a5de6b6dfc86259337d7925d9ad683bdf5
Amiron28 <fotolia505@gmail.com>
2022-05-03 22:27:05 +0500

    d/z

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
 let form = document.forms.register
-let block = document.querySelector('.block')
-let h1 = document.querySelectorAll('h1')
-let inputs = document.querySelectorAll('input')
-let ok = document.querySelector('.OK')
-let cansel = document.querySelector('.cansel')
-
-console.log(ok, cansel);
-
-form.onsubmit = (event) => {
-    event.preventDefault()
-
-    for (let item of inputs) {
-        if (item.value.length > 0) {
-            submit()
-            item.style.border = "2px solid green"
-            block.style.display = "block"
-
-        } else {
-            item.value.innerHTML = "eroor"
-            block.style.display = "none"
-            cansel.style.display = "block"
-            ok.style.display = "block"
-            item.style.border = "2px solid red"
-            break
-        }
-    }
-}
-
-ok.onclick = () => {
-    alert('отлично')
-}
-
-cansel.onclick = () => {
-    alert('понятно')
-    block.style.display = "none"
-    ok.style.display = "none"
-    cansel.style.display = "none"
-}
 
 function submit() {
     let user = {}
@@ -50,16 +12,3 @@ function submit() {
     showUser(user)
 }
 
-
-function showUser(data) {
-    let children = h1
-
-    for (let item of children) {
-        for (let key in data) {
-            if (item.getAttribute('name') == key) {
-                item.innerHTML = `${key}: ${data[key]}`
-            }
-        }
-    }
-
-}
