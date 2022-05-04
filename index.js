d67c9409f3722a75fa0e19f73d60e1375ebdc566
Amiron28 <fotolia505@gmail.com>
2022-05-04 15:08:12 +0500

    d/z

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
 let form = document.forms.register
+let inputs = form.querySelectorAll('input')
+
+let patterns = {
+    Name: /^[a-z ,.'-]+$/i,
+    Surname: /^[a-z ,.'-]+$/i,
+    Email: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+    Password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/
+}
+
+function validate(filed, regex) {
+    if (regex.test(filed.value)) {
+        filed.classList.add('valid')
+        filed.classList.remove('invalid')
+    } else {
+        filed.classList.add('invalid')
+        filed.classList.remove('valid')
+    }
+}
+
+inputs.forEach(inp => {
+    inp.onkeyup = () => {
+        validate(inp, patterns[inp.name])
+    }
+})
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    let arr = []
+
+    inputs.forEach(inp => {
+        if (inp.classList.value === 'invalid' || inp.value.length === 0) {
+            arr.push(1)
+        }
+    })
+
+    if (arr.length === 0) {
+        submit()
+    } else {
+        alert('error')
+    }
+}
+
 
 function submit() {
     let user = {}
@@ -9,6 +52,5 @@ function submit() {
         user[key] = value
     })
 
-    showUser(user)
+    console.log(user);
 }
-
