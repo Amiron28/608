63456fdfcf0401259cf9e2283393c9b4a68c56f2
Amiron28 <fotolia505@gmail.com>
2022-05-09 20:08:24 +0500

    d/z

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let body = document.body
 // let input = document.createElement('input') // создание
 
 
@@ -77,23 +76,126 @@ let body = document.body
 
 // })
 
-let arr = [
-    {
-        id: Math.random(),
-        course: 'FRONTEND',
-        days: "Вторник-Четверг-Суббота",
-        time: 13.30 - 15.00,
-        procent: 100
-    },
-    {
-        id: Math.random(),
-        course: 'Экстримальный Арабский язык',
-        days: "Вторник-Четверг-Суббота",
-        time: 13.30 - 15.00,
-        procent: 1
-    },
-]
+// let arr = [
+//     {
+//         id: Math.random(),
+//         course: 'FRONTEND',
+//         days: "Вторник-Четверг-Суббота",
+//         time: 13.30 - 15.00,
+//         procent: 100
+//     },
+//     {
+//         id: Math.random(),
+//         course: 'Экстримальный Арабский язык',
+//         days: "Вторник-Четверг-Суббота",
+//         time: 13.30 - 15.00,
+//         procent: 1
+//     },
+// ]
 
-for (let item of arr) {
-    console.log(item);
-}
+// for (let item of arr) {
+//     console.log(item);
+// }
+
+
+/-----------------------------------------------------------/
+
+
+// let arr = [
+//     'Все курсы',
+//     'Успейте записаться',
+//     'Преподаватели',
+//     'Фотографии',
+//     'Бесплатный урок',
+//     'Контакты',
+//     'UZ'
+// ]
+
+// let body = document.body
+
+// let header = document.createElement('header')
+// let left = document.createElement('div')
+// let right = document.createElement('div')
+// let img = document.createElement('img')
+// for (let item of arr) {
+//     let span = document.createElement('span')
+//     span.innerHTML = item
+
+//     left.append(span)
+// }
+
+
+
+// let button = document.createElement('button')
+// let brg = document.createElement('button')
+
+// button.innerHTML = "button"
+// brg.innerHTML = "burger"
+
+// left.classList.add('left')
+// right.classList.add('right')
+
+// img.src = "./logo.svg"
+// img.alt = "logo"
+
+// button.classList.add('btn')
+// brg.classList.add('btn')
+
+// header.append(left, right)
+// body.prepend(header)
+// right.append(button, brg)
+
+
+// /-----------------------------------------------------------------/
+
+let body = document.body
+
+// for (let i = 0; i <= 5; i++) {
+//     let main = document.createElement('div')
+//     main.classList.add('main')
+//     body.prepend(main)
+
+//     let logo = document.createElement('span')
+//     logo.classList.add('logo')
+//     logo.innerHTML = "Frontend"
+//     main.prepend(logo)
+
+//     let small_text = document.createElement('span')
+//     small_text.classList.add('text')
+//     small_text.innerHTML = "Осталось 10 мест"
+//     main.append(small_text)
+
+//     let polzunok = document.createElement('div')
+//     let fill = document.createElement('div')
+
+//     polzunok.classList.add('rate')
+//     fill.classList.add('fill')
+//     main.append(polzunok)
+//     polzunok.prepend(fill)
+
+//     let arr = [
+//         'Дни: Понедельник-среда-пятница',
+//         'Время: 13:30-15:00',
+//         'Длительность в месяцах: 4',
+//         'Цена: 515 000 сум/месяц',
+//         'Преподаватель: Абдурахмонов Сохиб'
+//     ]
+
+//     for (let item of arr) {
+//         let span = document.createElement('span')
+//         span.innerHTML = item
+//         span.classList.add('addition')
+//         main.append(span)
+
+//         span.style.margin = "20px"
+//     }
+
+//     let btn = document.createElement('button')
+//     btn.classList.add('btn')
+//     btn.innerHTML = "Записаться"
+//     main.append(btn)
+// }
+
+let container = document.createElement('div')
+container.classList.add('container')
+body.prepend(container)
